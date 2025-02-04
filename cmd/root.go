d141b693c55ecda0d47d81badc2dc12f9571ccab
Axel Aparicio Rotermund <axelaparicior@gmail.com>
2025-02-04 20:40:47 -0300

    Init project

---
+++ cmd/root.go
@@ -0,0 +1,55 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string = "gato.yaml"
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "gato",
+	Short: "Gato CLI is a request application similar to cURL",
+	Long:  `Gato CLI is a request application similar to cURL`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	viper.SetConfigFile(cfgFile)
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading config file:", err)
+	}
+}
---
+++ cmd/set.go
@@ -0,0 +1,60 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var host string
+var port int16
+var method string
+var body string
+
+// setCmd represents the set command
+var setCmd = &cobra.Command{
+	Use:   "set",
+	Short: "Command used to set different values needed in requests",
+	Long:  `This command will be used to set all different values needed`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if host != "" {
+			viper.GetViper().Set("host", host)
+			fmt.Println("Host changed to:", host)
+		}
+		if port >= 0 {
+			viper.GetViper().Set("port", port)
+			fmt.Println("Port changed to:", port)
+		}
+		if method != "" {
+			viper.GetViper().Set("method", method)
+			fmt.Println("Method changed to:", method)
+		}
+		if body != "" {
+			viper.GetViper().Set("body", body)
+			fmt.Println("Body changed to:", body)
+		}
+		_ = viper.GetViper().WriteConfig()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(setCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// setCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	setCmd.Flags().StringVarP(&host, "host", "H", "", "Set host name")
+	setCmd.Flags().Int16VarP(&port, "port", "P", -1, "Set port number")
+	setCmd.Flags().StringVarP(&method, "method", "M", "", "Set request method")
+	setCmd.Flags().StringVarP(&body, "body", "B", "", "Set body (json)")
+	setCmd.MarkFlagsOneRequired("host", "port", "method", "body")
+}
